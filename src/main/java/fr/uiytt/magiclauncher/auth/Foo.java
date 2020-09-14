f4c6e0e64da6b23c3a1094c212480027e26c31c0
uiytt <kahaneeden@protonmail.com>
2020-09-14 23:14:14 +0200

    Updated for a new server

---
+++ src/main/java/fr/uiytt/magiclauncher/auth/Foo.java
@@ -0,0 +1,51 @@
+package fr.uiytt.magiclauncher.auth;
+
+
+import javax.swing.JOptionPane;
+
+import fr.litarvan.openauth.AuthPoints;
+import fr.litarvan.openauth.AuthenticationException;
+import fr.litarvan.openauth.Authenticator;
+import fr.litarvan.openauth.model.AuthAgent;
+import fr.litarvan.openauth.model.response.AuthResponse;
+import fr.theshark34.openlauncherlib.minecraft.AuthInfos;
+import fr.uiytt.magiclauncher.Main;
+
+public class Foo implements Runnable {
+	private String username;
+	private boolean auth;
+
+	public Foo(String username, String password) {
+		this.auth = false;
+
+		this.username = username;
+		this.password = password;
+	}
+
+	private String password;
+
+	public void run() {
+		Authenticator authenticator = new Authenticator("https://authserver.mojang.com/",AuthPoints.NORMAL_AUTH_POINTS);
+		try {
+			AuthResponse response = authenticator.authenticate(AuthAgent.MINECRAFT, this.username, this.password,
+					"clientToken");
+			Auth.setAuthinfos(new AuthInfos(response.getSelectedProfile().getName(), response.getAccessToken(),
+					response.getSelectedProfile().getId()));
+		} catch (AuthenticationException e) {
+			Main.getFrame().getTextfieldpassword().setEnabled(true);
+			Main.getFrame().getTextfieldusername().setEnabled(true);
+			Main.getFrame().getConfigbutton().setEnabled(true);
+			Main.getFrame().getLaunchbutton().setEnabled(true);
+			JOptionPane.showMessageDialog(Main.getFrame().getLaunchbutton(),
+					"Impossible de se connecter :\n" + e.getMessage(), "Erreur - 2", 0);
+			this.auth = false;
+
+			return;
+		}
+		this.auth = true;
+	}
+
+	public boolean getAuth() {
+		return this.auth;
+	}
+}
---
+++ src/main/java/fr/uiytt/magiclauncher/update/PackInfos.java
@@ -0,0 +1,94 @@
+package fr.uiytt.magiclauncher.update;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.Scanner;
+import java.util.zip.CRC32;
+
+import de.leonhard.storage.Json;
+import fr.uiytt.magiclauncher.Config;
+import fr.uiytt.magiclauncher.Main;
+
+
+public class PackInfos {
+
+	public static void checkforversion() throws IOException {
+		Scanner s;
+
+			URL url;
+
+			if(Config.debug) {
+				url = new URL("https://raw.githubusercontent.com/uiytt/BrokenMagicLauncher/master/version.txt");
+			} else {
+
+				url = new URL(Config.link_VersionModPack);
+			}
+			s = new Scanner(url.openStream());
+
+
+
+		Config.link = s.nextLine();
+		Config.new_version = s.nextLine();
+		Config.CRC = s.nextLine();
+		s.close();
+		return;
+	}
+
+	public static boolean verify_modpack() {
+		Main.getFrame().getProgresstext().setText("Vérification des fichiers");
+		File downloadfile = new File(Config.DIR + "/launcher.json");
+		CRC32 crc = new CRC32();
+
+		if(!Config.debug) {
+			
+		
+		if (downloadfile.exists()) {
+			try {
+				crc.update(java.nio.file.Files.readAllBytes(downloadfile.toPath()));
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+		if(!downloadfile.exists() || crc.getValue() != Long.valueOf(Config.CRC)) {
+			Main.getFrame().getProgresstext().setText("Téléchargement de la liste des mods");
+			try {
+				URL website = new URL(Config.link);
+				ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+
+				FileOutputStream fos = new FileOutputStream(downloadfile);
+
+				fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+
+				fos.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}	
+		} 
+		}
+		Json jsondownloadlist = new Json("launcher",Config.DIR);
+		ModpackDownload modpack = new ModpackDownload(jsondownloadlist);
+		Main.getFrame().getSpeedtext().setVisible(true);
+		Main.getFrame().getResumbutton().setVisible(false);
+		Main.getFrame().getPausebutton().setVisible(true);
+		Main.getFrame().getStopbutton().setVisible(true);
+		return modpack.download();
+		
+		
+		
+
+	}
+
+	public static void deleteFolder(File element) {
+		if (element.isDirectory()) {
+			for (File sub : element.listFiles()) {
+				deleteFolder(sub);
+			}
+		}
+		element.delete();
+	}
+
+}
